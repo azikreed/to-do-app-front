3ed62208ae841770dd81f05f04fe938af0204e9d
azikreed <azikreed@gmail.com>
2024-03-26 17:10:09 +0300

    [+] delte status for using snackbar while deleting task

--- src/pages/TaskList/TaskList.tsx
+++ src/pages/TaskList/TaskList.tsx
@@ -4,22 +4,43 @@ import { TaskCard } from '../../components/TaskCard/TaskCard';
 import styles from './TaskList.module.css';
 import { AppDispatch, RootState } from '../../store/store';
 import { useEffect } from 'react';
-import { getTasks } from '../../store/task.slice';
+import { deleteTask, getTasks, taskActions } from '../../store/task.slice';
+import { Snackbar } from '../../components/Snackbar/Snackbar';
 
 export function TaskList() {
 	const dispatch = useDispatch<AppDispatch>();
-	const tasks = useSelector((s: RootState) => s.task.tasks);
+	const { tasks, deleteStatus } = useSelector((s: RootState) => s.task);
+
+	const deleteOne = async (id: string) => {
+		await dispatch(deleteTask(id));
+	};
 
 	useEffect(() => {
 		dispatch(getTasks());
 	}, [dispatch]);
 
+	useEffect(() => {
+		if (deleteStatus === 'success' || deleteStatus === 'error') {
+			const timer = setTimeout(() => {
+				dispatch(taskActions.clearDeleteStatus());
+			}, 10000); // Скрыть статус после 3 секунд
+
+			return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
+		}
+	}, [deleteStatus, dispatch]);
+
 	return (
 		<div className={styles['list']}>
-			<Headling appearance='small'>Таски</Headling>
-			{tasks?.map((task) => (
-				<TaskCard id={task._id}>{task.title}</TaskCard>
-			))}
+			<Headling appearance="small">Таски</Headling>
+			<div className={styles['tasks']}>
+				{tasks?.map((task) => (
+					<TaskCard key={task._id} id={task._id} onDelete={deleteOne}>
+						{task.title}
+					</TaskCard>
+				))}
+			</div>
+			{deleteStatus === 'success' && <Snackbar status='success' message="Удалено успешно" />}
+			{deleteStatus === 'error' && <Snackbar status='error' message="Ошибка при удалении" />}
 		</div>
 	);
 }
