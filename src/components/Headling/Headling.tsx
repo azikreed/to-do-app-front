76098bb2fe0878fc97396c66651a3ca7a133111e
azikreed <azikreed@gmail.com>
2024-03-26 17:09:22 +0300

    [*] fixed errors

--- src/components/Headling/Headling.tsx
+++ src/components/Headling/Headling.tsx
@@ -2,7 +2,7 @@ import styles from './Headling.module.css';
 import cn from 'classnames';
 import { HeadlingProps } from './Headling.props';
 
-function Headling({children, className, appearance, ...props}: HeadlingProps) {
+function Headling({children, className, appearance = 'small', ...props}: HeadlingProps) {
 	return (
 		<h1 className={cn(
 			className, styles['h1'],
