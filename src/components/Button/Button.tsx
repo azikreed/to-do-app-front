45f279a10df69db4e955ee258bd4b7b0723c060e
azikreed <azikreed@gmail.com>
2024-03-27 13:16:35 +0300

    [*] changed default appearance

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-function Button({ children, className, appearance ='small', ...props }: ButtonProps) {
+function Button({ children, className, appearance ='big', ...props }: ButtonProps) {
 	return (
 		<button
 			className={cn(styles['button'], styles['accent'], className,{
